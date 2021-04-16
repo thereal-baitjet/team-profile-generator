80a1e5440ab0995a2e6a768ffd14a400df8ff31b
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-16 16:06:38 -0400

    working on the index file

--- index.js
+++ index.js
@@ -4,3 +4,46 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
+
+const employees = [];
+
+function iniate() {
+    MakeHtml();
+    addTeamMemeber()
+}
+
+funtion addTeamMemeber() {
+    inquirer.prpomt([{
+        message: "Whats your employees' name?",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "whats your employees role?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager",
+        ],
+        name: "role",
+    },
+    {
+        messae: "whats your employees id",
+        name: "id",
+    },
+    {   message: "whats your employees emaiul adress",
+        name: "email"
+    }])
+    .then(function({name, role, id , email}){
+        let roleInfo = "";
+        if (role === "engineer") {
+            roleInfo = "GitHub usernamne";
+        } else if (role == "Intern") {
+            reoleInfo = "schoolename";
+         } else { 
+             roleInfo = "office number";
+        }
+        inquirer.prompt({[
+            message: `whats your team members'${roleInfo}`,
+            name: "roleInfo"
+        ]})
