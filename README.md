872ba942a556a5f8fcf254ca6d33f8033a6d0305
Juan Santos <62819163+thereal-baitjet@users.noreply.github.com>
2021-04-18 17:28:07 -0400

    finished the index.js and readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# team-profile-generator
+# team-profile-generator  ![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)
+  node project to auto generate a team profile using prompts questions
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ### Installation:
+  In order to install the necessary dependencies, open the console and run the following:
+  ```npm i inquirer```
+  ### Usage:
+  write in the command node index.js to run the program
+  ### License:
+  This project is licensed under:
+  MIT
+  ### Contributing:
+  contact me via email.
+  ### Tests:
+  In order to test open the console and run the following:
+  ```jest```
+  ### Questions:
+  If you have any questions contact me on [GitHub](https://github.com/Thereal-baitjet) or contact 
+  Juan Santos at baitjet@gmail.com
+  ![picture](https://github.com/Thereal-baitjet.png?size=80)
+   ### Credits 
+   https://www.linkedin.com/in/ryan-ellingson-25171614/ ryan Ellingson
+   ### badges
+  [![LinkedIn Badge](https://img.shields.io/badge/LinkedIn-Profile-informational?style=flat&logo=linkedin&logoColor=red&color=0D76A8)](https://www.linkedin.com/in/juan-santos-8380b0186/)
+
+
+  ![twitterbadge](https://img.shields.io/twitter/url?logoColor=red&style=social&url=https%3A%2F%2Ftwitter.com%2FBaitjet4)
+   
--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const Intern = require("./lib/intern");
 
 const employees = [];
 
-function iniate() {
-    MakeHtml();
-    addTeamMemeber()
+function initiate() {
+    makeHtml();
+    addTeamMember()
 }
 
-function addTeamMemeber() {
-    inquirer.prpomt([{
+function addTeamMember() {
+    inquirer.prompt([{
         message: "Whats your employees' name?",
         name: "name"
     },
     {
         type: "list",
-        message: "whats your employees role?",
+        message: "whats your employees' role?",
         choices: [
             "Engineer",
             "Intern",
@@ -28,54 +28,153 @@ function addTeamMemeber() {
         name: "role",
     },
     {
-        messae: "whats your employees id",
+        messae: "whats your employees' id",
         name: "id",
     },
-    {   message: "whats your employees emaiul adress",
+    {
+        message: "whats your employees' email address",
         name: "email"
     }])
 
-    .then(function({name, role, id , email}){
-        let roleInfo = "";
-        if (role === "engineer") {
-            roleInfo = "GitHub usernamne";
-        } else if (role == "Intern") {
-            reoleInfo = "schoolename";
-         } else { 
-             roleInfo = "office number";
-        }
-        inquirer.prompt({
-            message: `whats your team members'${roleInfo}`,
-            name: "roleInfo"
-        },
-        {
-            type: "list",
-            message: "Would you like to add more team members?",
-            choices: [
-                "yes",
-                "no"
-            ],
-            name: "moreMembers"
-        })
-        .then(function({roleInfo, moreMembers}) {
-            let newMember;
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
             if (role === "Engineer") {
-                newMember = new Engineer(name, id, email, roleInfo);
-            } else if (role === "Intern") {
-                newMember = new Intern(name, id, email, roleInfo);
-            } else {
-                newMember = new Manager(name, id, email, roleInfo);
+                roleInfo = "GitHub usernamne";
+            } else if (role == "Intern") {
+                roleInfo = "school name";
+            } else if (role === "Manager") {
+                roleInfo = "office number";
             }
-            employees.push(newMember);
-            addHtml(newMember)
-            .then(function() {
-                if (moreMembers === "yes") {
-                    addMember();
-                } else {
-                    finishHtml();
-                }
-            });
-            
+            inquirer.prompt([{
+                message: `whats your employees' ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no",
+                ],
+                name: "moreMembers"
+            }])
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else if (role === "Manager") {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    addHtml(newMember)
+                        .then(function () {
+                            if (moreMembers === "yes") {
+                                addTeamMember();
+                            } else {
+                                finishHtml();
+                            }
+                        });
+
+                });
+        });
+}
+function makeHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body style="background: lightblue;">
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
+function addHtml(member) {
+    return new Promise(function (resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-item">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-item">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+                <ul class="list-group list-group-item">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+                </ul>
+                </div>
+            </div>
+            </div>
+        </div>
+    
+    </body>
+    </html>`
+        }
+        console.log("team created");
+        fs.appendFile("./team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
         });
     });
 }
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+initiate();
